95b9c6d954e27951bcf8c8ef9b5ff340b1d3e4e8
saho-london <kate.eighth@gmail.com>
2017-07-06 22:11:11 +0900

    patatokukasi

--- python-flask/main.py
+++ python-flask/main.py
@@ -5,7 +5,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def root():
-    return render_template("hello.html")
+
+    pato = request.form.get('a', u"パトカー")
+    taku = request.form.get('b', u"タクシー")
+    
+    pata = ""
+    
+    for i in range(min(len(pato), len(taku))):
+        pata = pata + pato[i]
+        pata = pata + taku[i]
+    
+    return render_template("hello.html", pata=pata)
+
 
